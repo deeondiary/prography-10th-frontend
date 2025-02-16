588fdc8941ad07085e03b3b59a4c120b37de18b4
Jiwon Choi <jiwon94k@gmail.com>
2025-02-16 10:20:22 +0900

    design : COLORS 상수 추가

---
+++ src/constants/constants.tsx
@@ -0,0 +1,9 @@
+export const COLORS = {
+    PINK: '#FA2454',
+    PURPLE: '#B252FF',
+    ORANGE: '#FF6929',
+    YELLOW: '#FFB903',
+    BLUE: '#1034FF',
+    GR_DARK: '#999999',
+    GR_LIGHT: '#F9F9F9',
+} as const;

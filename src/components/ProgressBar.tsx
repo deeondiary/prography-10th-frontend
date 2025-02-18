cf17c61c8f4fe9af5e93be29cb4ca8467ca4d514
Jiwon Choi <jiwon94k@gmail.com>
2025-02-18 18:37:15 +0900

    feat : 공통 컴포넌트 > 설문 progress bar 생성

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import Button from "./Button.tsx";
+import {COLORS} from "../global/constants.tsx";
+import useFormContext from "../hooks/useFormContext.tsx";
+import {FormContextType} from "../global/form.interface.tsx";
+
+function ProgressBar() {
+    const { page } = useFormContext() as FormContextType;
+    return (
+        <BarWrap>
+            <Button disabled={true} size='s' borderRound={true} color={page === 0 ? COLORS.PINK : COLORS.GR_LIGHT}>1.수집동의</Button>
+            <CenterLine />
+            <Button disabled={true} size='s' borderRound={true} color={page === 1 ? COLORS.PINK : COLORS.GR_LIGHT}>2.기본정보</Button>
+            <CenterLine />
+            <Button disabled={true} size='s' borderRound={true} color={page === 2 ? COLORS.PINK : COLORS.GR_LIGHT}>3.지원정보</Button>
+        </BarWrap>
+    );
+}
+
+const BarWrap = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 24px 0 42px 0;
+`
+const CenterLine = styled.div`
+    background-color: ${COLORS.GR_LIGHT};
+    height: 2px;
+    width: 100%;
+`
+export default ProgressBar;
